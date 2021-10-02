b05dc1c124a96eac4c8a00e6a2f81ac156e835fa
luanpires94 <luan.94pires@gmail.com>
2021-10-02 19:11:55 -0300

    visual update of modal buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import Modal from 'react-modal'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
-import { Container } from './styles'
+
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -8,6 +12,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+
+    const [type, setType] = useState('deposit')
 
     return (
         <div>
@@ -34,6 +40,28 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         type="number"
                         placeholder="Valor"
                     />
+
+                    <TransactionTypeContainer>
+                        <RadioBox
+                            type="button"
+                            onClick={() => { setType('deposit') }}
+                            isActive={type === 'deposit'}
+                            activeColor="green"
+                        >
+                            <img src={incomeImg} alt="" />
+                            <span>Entrada</span>
+                        </RadioBox>
+
+                        <RadioBox
+                            type="button"
+                            onClick={() => { setType('withdraw') }}
+                            isActive={type === 'withdraw'}
+                            activeColor="red"
+                        >
+                            <img src={outcomeImg} alt="" />
+                            <span>Saída</span>
+                        </RadioBox>
+                    </TransactionTypeContainer>
 
                     <input
                         placeholder="Categoria"
